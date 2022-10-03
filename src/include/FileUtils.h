71d79629a5c1f170ef9bc3053d31f2e91fbb4abe
Isadora Gonçalves <isadora.goncalves@estudante.ufjf.br>
2022-10-03 20:15:39 -0300

    Feat: Adiciona entrada e saída de arquivo.

--- src/include/FileUtils.h
+++ src/include/FileUtils.h
@@ -1,9 +1,59 @@
 #include <iostream>
-#include <sstream>
+#include <fstream> // lib de file stream
+#include <sstream> // lib de string stream
+
 using namespace std;
 
-void readFile(string filename, string &content, string fileExt)
-{}
+string readFile(string filename, string fileExt)
+{
+  ifstream inFile;
+  string line, value, fileContent = "";
+
+  inFile.open(filename + fileExt, ios::in);
+
+  if (!inFile.is_open())
+  {
+    cout << "Error opening file: " << filename << fileExt << endl;
+    return;
+  }
+
+  while (!inFile.eof())
+  {
+    getline(inFile, line);
+    istringstream iss(line); // separa os valores da linha
+                             // lida com os valores:
+    while (iss >> value)
+    {
+      fileContent += value;
+    }
+  }
+
+  inFile.close();
+
+  return fileContent;
+}
 
 void writeFile(string filename, string content, string fileExt)
+{
+  ofstream outFile;
+
+  outFile.open(filename + fileExt, ios::out);
+
+  if (!outFile.is_open())
+  {
+    cout << "Error opening file: " << filename << fileExt << endl;
+    return;
+  }
+
+  if (fileExt.compare(".dot") == 0)
+  {
+    formatDotFile(content);
+  }
+
+  outFile.write(content.c_str(), content.length());
+
+  outFile.close();
+}
+
+string formatDotFile(string content)
 {}
